4b5acb846652467600f9fc3867aedd51d559037b
Eageon <yujun.eageon@gmail.com>
2014-02-23 22:05:11 -0600

    Markov build completed

--- src/GraphicalModel.java
+++ src/GraphicalModel.java
@@ -42,7 +42,7 @@ public class GraphicalModel {
 		factors.set(index, factor);
 	}
 
-	@SuppressWarnings({ "null", "unused" })
+	// @SuppressWarnings({ "null", "unused" })
 	private void buildMarkovNetwork(BufferedReader reader)
 			throws NumberFormatException, IOException {
 		int size = Integer.valueOf(reader.readLine());
@@ -60,17 +60,21 @@ public class GraphicalModel {
 		}
 
 		String line4 = reader.readLine();
-		int numFunctions = Integer.valueOf(line4);
-		factors = new ArrayList<>(numFunctions);
+		int numFactors = Integer.valueOf(line4);
+		factors = new ArrayList<>(numFactors);
 
 		// initialize factors without setting the table
-		String funcLine = null;
-		while (null != (funcLine = reader.readLine()) && !funcLine.equals("")) {
-			String[] args = funcLine.split(" ");
+		String factorLine = null;
+		while (null != (factorLine = reader.readLine())
+				&& !factorLine.equals("")) {
+			String[] args = factorLine.split("\t| ");
 			if (2 > args.length)
 				continue;
+			int indexLastVariable = Integer.valueOf(args[args.length - 1]);
 			int numScopes = Integer.valueOf(args[0]);
+			// variables.get(indexLastVariable).domainSize();
 			Factor factor = new Factor(numScopes);
+			factors.add(factor);
 
 			for (int i = 1; i < args.length; i++) {
 				int indexVaraible = Integer.valueOf(args[i]);
@@ -78,12 +82,53 @@ public class GraphicalModel {
 			}
 		}
 
-		if (null == funcLine) {
+		if (null == factorLine) {
 			System.out.println("File Format problem");
 			System.exit(-1);
 		}
 
 		// Then set CPT
+		int indexFactor = 0;
+		String head = null;
+		int actualCount = 0;
+		while (null != (head = reader.readLine())) {
+			if (head.equals("")) {
+				continue; // escape the newline
+			}
+			actualCount++;
+
+			int numCells = Integer.valueOf(head);
+			Factor factor = factors.get(indexFactor++);
+			factor.initTable(numCells);
+
+			int numLinesToRead = numCells / factor.numColomns();
+			String tableRow = null;
+			
+			tableRow = reader.readLine();
+			if (null == tableRow || tableRow.equals("")) {
+				System.out.println("Function row format error: less lines");
+				System.exit(-1);
+			}
+
+			tableRow = tableRow.trim();
+			String[] tableRowValues = tableRow.split("\t| ");
+			if (tableRowValues.length != numCells) {
+				System.out.println("Function row format error: less colomn");
+				System.exit(-1);
+
+			}
+
+			for (int j = 0; j < tableRowValues.length; j++) {
+				factor.setTableValue(j,
+						Double.valueOf(tableRowValues[j]));
+			}
+		}
+
+		if (actualCount != numFactors) {
+			System.out
+					.println("Format error: actual Factor data less the preamble");
+			System.exit(-1);
+		}
 	}
 
 	private void buildBayesianNetwork(BufferedReader reader)
@@ -115,10 +160,9 @@ public class GraphicalModel {
 				continue;
 			int indexLastVariable = Integer.valueOf(args[args.length - 1]);
 			int numScopes = Integer.valueOf(args[0]);
-			//variables.get(indexLastVariable).domainSize();
+			// variables.get(indexLastVariable).domainSize();
 			Factor factor = new Factor(numScopes);
 			factors.add(factor);
-			
 
 			for (int i = 1; i < args.length; i++) {
 				int indexVaraible = Integer.valueOf(args[i]);
@@ -134,27 +178,35 @@ public class GraphicalModel {
 		// Then set CPT
 		int indexFactor = 0;
 		String head = null;
+		int actualCount = 0;
 		while (null != (head = reader.readLine())) {
+			if (head.equals("")) {
+				continue; // escape the newline
+			}
+			actualCount++;
+
 			int numCells = Integer.valueOf(head);
-			
+
 			Factor factor = factors.get(indexFactor++);
 			factor.initTable(numCells);
-			
+
 			int numLinesToRead = numCells / factor.numColomns();
-			
 
 			String tableRow = null;
 			int i = 0;
 			for (i = 0; i < numLinesToRead; i++) {
 				tableRow = reader.readLine();
-				if (null == tableRow || tableRow.equals(""))
-					break;
+				if (null == tableRow || tableRow.equals("")) {
+					System.out.println("Factor row format error: less lines");
+					System.exit(-1);
+				}
 
 				tableRow = tableRow.trim();
 				String[] tableRowValues = tableRow.split("\t| ");
 				if (tableRowValues.length != factor.numColomns()) {
-					System.out.println("Factor row format error");
+					System.out.println("Factor row format error: less colomn");
 					System.exit(-1);
+
 				}
 
 				for (int j = 0; j < tableRowValues.length; j++) {
@@ -162,17 +214,18 @@ public class GraphicalModel {
 							Double.valueOf(tableRowValues[j]));
 				}
 			}
-			
+
 			// judge the validity of the file
-			if(i != numLinesToRead) {
+			if (i != numLinesToRead) {
 				System.out.println("Factor " + i + ": wrong CPT format");
 				System.exit(-1);
 			}
-			tableRow = reader.readLine();
-			if(!tableRow.equals("")) {
-				System.out.println("CPT Not separated by new line");
-				System.exit(-1);
-			}
+		}
+
+		if (actualCount != numFactors) {
+			System.out
+					.println("Format error: actual Factor data less the preamble");
+			System.exit(-1);
 		}
 	}
 
@@ -195,14 +248,26 @@ public class GraphicalModel {
 				System.out.println("Wrong preamble " + network);
 			}
 
+			reader.close();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
-	
+
+	public void readEvidence(String fileName) {
+		BufferedReader reader = null;
+		try {
+			reader = new BufferedReader(new FileReader(fileName));
+		} catch (FileNotFoundException e) {
+			System.out.println("Can NOT find file " + fileName);
+			System.exit(-1);
+		}
+
+	}
+
 	public static void main(String[] args) {
-		String fileName = "/home/leageon/GraphicalModel/hw2-problems/BN_8.uai";
-		
+		String fileName = "/home/leageon/GraphicalModel/hw2-problems/network.uai";
+
 		GraphicalModel model = new GraphicalModel(fileName);
 		System.out.println("Succeed!");
 	}
